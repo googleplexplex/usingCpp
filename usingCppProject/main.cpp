f92388b1adc5542b47e6708c9d42ab5823e38261
googleplexplex <iambagger10@gmail.com>
2020-08-24 22:42:28 +0300

    Revert "Добавлен исходный код и примеры"
    This reverts commit 533b2d51cbf1171ba5cce58aad6715205ced7155.

--- usingCppProject/main.cpp
+++ usingCppProject/main.cpp
@@ -1,37 +1,10 @@
 #include <iostream>
-#include <fstream>
 #include "using.hpp"
 using namespace std;
 
-
-class fileWithUsing : public ofstream, public IInsertableInUsing
-{
-public:
-	string fileName;
-
-	fileWithUsing(const string& fileName)
-	{
-		this->fileName = fileName;
-	}
-
-	void onStart()
-	{
-		open(fileName);
-	}
-
-	void onEnd()
-	{
-		close();
-	}
-};
-
-
 int main()
 {
-	using(fileWithUsing, example, ("helloFile.txt"),
-	{
-		example << "Hello, using!";
-	});
+	//...
 
 	return 0;
 }
--- usingCppProject/using.hpp
+++ usingCppProject/using.hpp
@@ -1,16 +1,3 @@
 #pragma once
 
-class IInsertableInUsing
-{
-public:
-	virtual void onStart() = 0;
-	virtual void onEnd() = 0;
-};
-
-#define using(type, name, constr, code) \
-{\
-type name constr;\
-name.onStart();\
-code \
-name.onEnd(); \
-}
+//...
